c92303c7cb9e75f9c5a6ef2d7647d0644bac65a2
4248che <100670060+4248che@users.noreply.github.com>
2022-03-31 06:48:18 +0800

    40725093-5

--- WebGame_2022-03-01/Assets/Scripts/TopDownController.cs
+++ WebGame_2022-03-01/Assets/Scripts/TopDownController.cs
@@ -5,11 +5,14 @@ namespace GG
 	public class TopDownController : MonoBehaviour
 	{
 		#region CC
+		[SerializeField, Header("speed"), Range(0, 100)]
 		private float speed = 10.5f;
-		private string parameterRun = "";
-		private string parameterDead = "";
+		private string parameterRun = "Idle";
+		private string parameterDead = "roll";
 		private Animator ani;
 		private Rigidbody2D rig;
+		private float h;
+		private float v;
 		#endregion
 		
 		#region CC1
@@ -23,11 +26,30 @@ namespace GG
 		
 		private void Update()
 		{
-			print("Dbug~");
+			// print("Dbug~");
+			
+			GetInput();
+			Move();
 		}
 		#endregion
 		
 		#region CC2
+		private void GetInput()
+		{
+			h = Input.GetAxis("Horizontal");
+			v = Input.GetAxis("Vertical");
+			
+			print("LINE : " + h );
+		}
+		
+		private void Move()
+		{
+			rig.velocity = new Vector2(h,v) * speed;
+			
+			ani.SetBool(parameterRun, h != 0 || v !=0);
+			
+			transform.eulerAngles = new Vector3(0, h >= 0 ? 0 : 180, 0);
+		}
 		#endregion
 	}
 
